65b3a23dd4de427770db90c8d3363df46155ee71
Mohamad Sharafeddine <mhsharafeddine@gmail.com>
2024-07-07 17:49:00 +0300

    Imported OS

--- client.py
+++ client.py
@@ -1,7 +1,11 @@
 import yaml
+import os
 
 class Client:
-    def __init__(self, config_path='\config.yaml'):
+    def __init__(self, config_path=None):
+        if config_path is None:
+            # Default path relative to the current file
+            config_path = os.path.join(os.path.dirname(__file__), '../config.yaml')
         with open(config_path, 'r') as file:
             self.config = yaml.safe_load(file)
         self.server_ip = self.config['ServerIPAddress']
